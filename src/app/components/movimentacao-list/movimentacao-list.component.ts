29fb7c2eea836090a831191341a044a4cc81f28e
Leody de Carvalho <leody100@gmail.com>
2022-05-10 00:09:01 -0300

    criando componentes

---
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+@Component({
+  selector: 'app-movimentacao-list',
+  templateUrl: './movimentacao-list.component.html',
+  styleUrls: ['./movimentacao-list.component.css']
+})
+export class MovimentacaoListComponent implements OnInit {
+  movimentacoes: any;
+  constructor(private movimentacaoService: MovimentacaoService) { }
+
+  ngOnInit(): void {
+  }
+
+}
