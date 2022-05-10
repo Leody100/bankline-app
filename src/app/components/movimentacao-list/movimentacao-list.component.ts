69ae20a5a048eb5c677108129e00c26795476fef
Leody de Carvalho <leody100@gmail.com>
2022-05-10 18:46:45 -0300

    finalizando projeto funcionalidades

--- src/app/components/movimentacao-list/movimentacao-list.component.ts
+++ src/app/components/movimentacao-list/movimentacao-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CorrentistaService } from 'src/app/services/correntista.service';
 import { MovimentacaoService } from 'src/app/services/movimentacao.service';
+
+
 @Component({
   selector: 'app-movimentacao-list',
   templateUrl: './movimentacao-list.component.html',
@@ -7,9 +10,38 @@ import { MovimentacaoService } from 'src/app/services/movimentacao.service';
 })
 export class MovimentacaoListComponent implements OnInit {
   movimentacoes: any;
-  constructor(private movimentacaoService: MovimentacaoService) { }
+  correntistas: any;
+  correntista: any;
+
+  constructor(
+    private movimentacaoService: MovimentacaoService,
+    private correntistaService: CorrentistaService
+  ) { }
 
   ngOnInit(): void {
+    this.exibirCorrentistas();
+  }
+  exibirCorrentistas(): void {
+    this.correntistaService.list()
+      .subscribe(
+        data => {
+          this.correntistas = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  listMovimentacoes(): void {
+    this.movimentacaoService.findByIdConta(this.correntista.id)
+      .subscribe(
+        data => {
+          this.movimentacoes = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
 
 }
